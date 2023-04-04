04ccf5a395056c390681adc27b7ad9e6d6dbbfb4
unknown <yohei.hokari@gmail.com>
2023-04-05 08:10:34 +0900

    first commit

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+import { Sample } from './Sample';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client} >
+      <Sample />
+    </ApolloProvider>
+  );
+}
+
+export default App;
